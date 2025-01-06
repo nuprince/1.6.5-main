947190d5056d658d40ff7b204c2c8a509e9ec6f1
nuprince <jhillnj@gmail.com>
2025-01-06 17:20:31 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,16 +2,17 @@
 # exit on error
 set -o errexit
 
+# Install Node dependencies in assets directory
+cd assets && npm install && cd ..
+
 # Initial setup
 mix deps.get --only prod
 MIX_ENV=prod mix compile
 
 # Compile assets
-# Make sure tailwind and esbuild are installed
 MIX_ENV=prod mix assets.build
-# Build minified assets
 MIX_ENV=prod mix assets.deploy
 
-# Create server script, Build the release, and overwrite the existing release directory
+# Create server script and build release
 MIX_ENV=prod mix phx.gen.release
 MIX_ENV=prod mix release --overwrite
