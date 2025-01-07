96228e37a0f1b7dc85efa2f8d3e5532879ee9e0b
nuprince <jhillnj@gmail.com>
2025-01-06 19:22:23 -0500

    Update releases.exs

--- config/releases.exs
+++ config/releases.exs
@@ -1,63 +1,45 @@
 # In this file, we load production configuration and secrets
-# from environment variables. You can also hardcode secrets,
-# although such is generally not recommended and you have to
-# remember to add this file to your .gitignore.
+# from environment variables.
 import Config
 
 ### Database: DATABASE_URL / POOL_SIZE ###
 database_url =
-  System.get_env("DATABASE_URL") ||
-    raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
+ System.get_env("DATABASE_URL") ||
+   raise """
+   environment variable DATABASE_URL is missing.
+   For example: ecto://USER:PASS@HOST/DATABASE
+   """
 
 config :spades, Spades.Repo,
-  # ssl: true,
-  url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+ ssl: true,
+ url: database_url,
+ pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 ### SECRET_KEY_BASE ###
 secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
+ System.get_env("SECRET_KEY_BASE") ||
+   raise """
+   environment variable SECRET_KEY_BASE is missing.
+   You can generate one by calling: mix phx.gen.secret
+   """
 
 config :spades, SpadesWeb.Endpoint,
-  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
-  secret_key_base: secret_key_base
+ http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
+ secret_key_base: secret_key_base
 
-### Mail: MAILGUN_API_KEY / MAILGUN_DOMAIN###
-mailgun_api_key =
-  System.get_env("MAILGUN_API_KEY") ||
-    raise """
-    environment variable MAILGUN_API_KEY is missing.
-    """
-
-mailgun_domain =
-  System.get_env("MAILGUN_DOMAIN") ||
-    raise """
-    environment variable MAILGUN_DOMAIN is missing.
-    """
+### Mail: MAILGUN_API_KEY / MAILGUN_DOMAIN (Optional) ###
+mailgun_api_key = System.get_env("MAILGUN_API_KEY") || "dummy_key"
+mailgun_domain = System.get_env("MAILGUN_DOMAIN") || "dummy_domain"
 
 config :spades, Spades.Mailer,
-  adapter: Swoosh.Adapters.Mailgun,
-  api_key: mailgun_api_key,
-  domain: mailgun_domain
+ adapter: Swoosh.Adapters.Mailgun,
+ api_key: mailgun_api_key,
+ domain: mailgun_domain
 
 config :spades, SpadesWeb.PowMailer,
-  adapter: Swoosh.Adapters.Mailgun,
-  api_key: mailgun_api_key,
-  domain: mailgun_domain
+ adapter: Swoosh.Adapters.Mailgun,
+ api_key: mailgun_api_key,
+ domain: mailgun_domain
 
-# ## Using releases (Elixir v1.9+)
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start each relevant endpoint:
-#
+# Start Phoenix endpoint
 config :spades, SpadesWeb.Endpoint, server: true
-#
-# Then you can assemble a release by calling `mix release`.
-# See `mix help release` for more information.
