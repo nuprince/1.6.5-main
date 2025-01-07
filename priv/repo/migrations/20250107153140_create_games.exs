1ac01cd9f045a6e07b94595f4c3a710e58c2292a
nuprince <jhillnj@gmail.com>
2025-01-07 10:43:30 -0500

    Update 20250107153140_create_games.exs

--- priv/repo/migrations/20250107153140_create_games.exs
+++ priv/repo/migrations/20250107153140_create_games.exs
@@ -1,10 +1,1 @@
-defmodule Spades.Repo.Migrations.CreateGames do
-  use Ecto.Migration
 
-  def change do
-    create table(:games) do
-      add :is_started, :boolean, default: false
-      timestamps()
-    end
-  end
-end
