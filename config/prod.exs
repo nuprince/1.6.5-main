930369bffaaf3e66606738839c86500e67771491
nuprince <jhillnj@gmail.com>
2025-01-07 15:44:53 -0500

    Update prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -4,7 +4,7 @@ config :spades, SpadesWeb.Endpoint,
  url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80],
  cache_static_manifest: "priv/static/cache_manifest.json",
  check_origin: [
-   "https://prospades.vercel.app",
+   "https://one-6-5-main-2.onrender.com",
    
    "http://localhost:3000"
  ],
