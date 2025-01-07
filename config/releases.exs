71a29d5bcfb9f7b9fe52456f4344560b3ad93fdc
nuprince <jhillnj@gmail.com>
2025-01-06 19:34:12 -0500

    Update releases.exs

--- config/releases.exs
+++ config/releases.exs
@@ -12,6 +12,7 @@ database_url =
 
 config :spades, Spades.Repo,
  ssl: true,
+ ssl_opts: [verify: :verify_none],
  url: database_url,
  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
