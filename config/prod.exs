85d016a76b1e3e4846eb73a31838b1c9340df035
nuprince <jhillnj@gmail.com>
2025-01-07 22:37:00 -0500

    Update prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -5,7 +5,8 @@ config :spades, SpadesWeb.Endpoint,
  cache_static_manifest: "priv/static/cache_manifest.json",
  check_origin: [
    "http://localhost:3000",
- "https://one-6-5-main-2.onrender.com"
+ "https://one-6-5-main-2.onrender.com",
+"https://prospades.vercel.app"
  ],
  front_end_email_confirm_url: "http://starspades.com/confirm-email/{token}",
  front_end_reset_password_url: "http://starspades.com/reset-password/{token}"
